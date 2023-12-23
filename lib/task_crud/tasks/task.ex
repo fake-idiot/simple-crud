09359a0806ea6820a613674ce9618c9f550a8d14
Muhammad Hamza <hamzarakha786@gmail.com>
2023-12-23 15:30:59 +0500

    Updated task schema

--- lib/task_crud/tasks/task.ex
+++ lib/task_crud/tasks/task.ex
@@ -5,6 +5,9 @@ defmodule TaskCrud.Tasks.Task do
   schema "tasks" do
     field :description, :string
     field :title, :string
+    field :status, Ecto.Enum, values: [:todo, :in_progress, :completed]
+    field :priority, Ecto.Enum, values: [:high, :medium, :low]
+    field :due_date, :utc_datetime
 
     timestamps(type: :utc_datetime)
   end
@@ -12,7 +15,7 @@ defmodule TaskCrud.Tasks.Task do
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :description])
-    |> validate_required([:title, :description])
+    |> cast(attrs, [:title, :description, :status, :priority])
+    |> validate_required([:title, :description, :status, :priority])
   end
 end
--- priv/repo/migrations/20231223102115_create_tasks.exs
+++ priv/repo/migrations/20231223102115_create_tasks.exs
@@ -3,10 +3,15 @@ defmodule TaskCrud.Repo.Migrations.CreateTasks do
 
   def change do
     create table(:tasks) do
+      add :description, :text
       add :title, :string
-      add :description, :string
+      add :status, :string
+      add :priority, :string
+      add :due_date, :utc_datetime
 
       timestamps(type: :utc_datetime)
     end
+
+    create unique_index(:tasks, [:title])
   end
 end
